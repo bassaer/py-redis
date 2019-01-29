77417f9a021766d2daebeb2f352eaf1c3f7d980f
Tsubasa Nakayama <app.nakayama@gmail.com>
2019-01-29 12:07:32 +0000

    init

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import redis
 
-if __name__ == '__main__':
+
+def read():
     r = redis.StrictRedis(host='localhost', port=6379, db=0)
     r.set("hello", "world")
     print("hello %s!" % r.get("hello").decode())
+
+
+if __name__ == '__main__':
+   read() 
